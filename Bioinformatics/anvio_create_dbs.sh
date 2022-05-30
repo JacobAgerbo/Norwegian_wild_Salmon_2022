1ffe764802e0f7a2b31e6298bda90ddd4d6a7357
Jacob Agerbo Rasmussen <Genomicsisawesome@gmail.com>
2022-05-30 14:42:20 +0200

    .

--- Bioinformatics/anvio_create_dbs.sh
+++ Bioinformatics/anvio_create_dbs.sh
@@ -8,9 +8,6 @@ anvi-gen-contigs-database -f contigs-fixed.fa -o CONTIGS.db -n 'CONTIG DB of Nor
 # Merge Profiles
 anvi-merge $PROFILES/*/PROFILE.db -o $PROFILES/MERGED_Profile --enforce-hierarchical-clustering -c $CONTIGS/CONTIGS.db
 
-# Use CONCOCT
-anvi-cluster-contigs -p $PROFILES/MERGED_Profile/PROFILE.db -c $CONTIGS/CONTIGS.db -C CONCOCT --driver concoct --just-do-it -T 10
-
 ### Create hidden Markov models
 for db in CONTIGS.db
   do
@@ -32,9 +29,8 @@ kaiju -t $kaiju_path/nodes.dmp \
       -f $kaiju_path/kaiju_db_nr_euk.fmi \
       -i gene_calls.fa \
       -o gene_calls_nr.out \
-      -z 40 \
+      -z 10 \
       -v
-
 #
 addTaxonNames -t $kaiju_path/nodes.dmp \
               -n $kaiju_path/names.dmp \
@@ -47,9 +43,13 @@ anvi-import-taxonomy-for-genes -i gene_calls_nr.names \
                                  -p kaiju \
                                  --just-do-it
 
+#
+# Use CONCOCT
+anvi-cluster-contigs -p $PROFILES/MERGED_Profile/PROFILE.db -c $CONTIGS/CONTIGS.db -C CONCOCT --driver concoct --just-do-it -T 10
+
 # Make MAG calling
-anvi-rename-bins -c CONTIGS.db \
-               -p PROFILE.db \
+anvi-rename-bins -c $CONTIGS/CONTIGS.db \
+               -p $PROFILES/PROFILE.db \
                --prefix NWS \
                --collection-to-read CONCOCT \
                --collection-to-write MAGs \
